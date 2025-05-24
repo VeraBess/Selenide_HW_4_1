73549e76b29fe7bfa21162a63cbcaba5371705cf
vera bessonova <v2112365@gmail.com>
2025-05-24 20:57:09 +0300

    Tests for task 2

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,6 +1,5 @@
 package ru.netology;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,6 +9,9 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
@@ -34,7 +36,44 @@ public class CardDeliveryTest {
         $("[data-test-id='agreement']").click();
         $("button.button").click();
         $(".notification__content")
-                .should(Condition.visible, Duration.ofSeconds(15))
-                .should(Condition.text("Встреча успешно забронирована на " + planningDate));
+                .should(visible, Duration.ofSeconds(15))
+                .should(text("Встреча успешно забронирована на " + planningDate));
+    }
+
+    @Test
+    public void sendigFormValidDataSelectDateInCalendar() {
+        $("[data-test-id='city'] input").setValue("Москва");
+
+        //выбор даты из календаря
+        $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);  //очищаем поле календаря
+        $("button").click(); // Нажатие на иконку календаря
+        $("[data-test-id='date'] input").should(visible, Duration.ofSeconds(15));
+        String planningDate = LocalDate.now().plusDays(7).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));//добавляем к текущей дате неделю и кладем в переменную planningDate и приводим к нужному формату
+        $("[data-test-id='date'] input").setValue(planningDate); //вводим планируемую дату в поле
+
+        $("[data-test-id='name'] input").setValue("Иванов Иван Иванович");
+        $("[data-test-id='phone'] input").setValue("+71234567890");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .should(visible, Duration.ofSeconds(15))
+                .should(text("Встреча успешно забронирована на " + planningDate));
+    }
+
+    @Test
+    public void sendigFormValidDataDropDownList() {
+        //выбор города из выпадающего списка
+        $("[data-test-id='city'] input").setValue("Мо");
+        $(".input__menu").find(byText("Москва")).should(visible, Duration.ofSeconds(15)).click();
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Иванов Иван Иванович");
+        $("[data-test-id='phone'] input").setValue("+71234567890");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .should(visible, Duration.ofSeconds(15))
+                .should(text("Встреча успешно забронирована на " + planningDate));
     }
 }
