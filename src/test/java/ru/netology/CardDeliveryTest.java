1cd91a43032f3fbff79b348b5662fc34e5a565c9
vera bessonova <v2112365@gmail.com>
2025-06-02 22:53:47 +0300

    solution task 2

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -17,6 +17,7 @@ public class CardDeliveryTest {
 
     @BeforeEach
     void setup() {
+
         Selenide.open("http://localhost:9999");
     }
 
@@ -40,29 +41,17 @@ public class CardDeliveryTest {
     }
 
     @Test
-    public void sendigFormValidDataSelectDateInCalendar() { //выбор даты из календаря
-        $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);  //очищаем поле календаря
-        $("button").click(); // Нажатие на иконку календаря
-        $("[data-test-id='date'] input").should(visible, Duration.ofSeconds(15));
-        String planningDate = LocalDate.now().plusDays(6).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));//добавляем к текущей дате неделю и кладем в переменную planningDate и приводим к нужному формату
-        $$(".calendar__day").find(exactText("31")).click();
-        $("[data-test-id='name'] input").setValue("Иванов Иван Иванович");
-        $("[data-test-id='phone'] input").setValue("+71234567890");
-        $("[data-test-id='agreement']").click();
-        $("button.button").click();
-        $(".notification__content")
-                .should(visible, Duration.ofSeconds(15))
-                .should(text("Встреча успешно забронирована на " + planningDate));
-    }
-
-    @Test
-    public void sendigFormValidDataDropDownList() {  //выбор города из выпадающего списка
+    public void sendigFormValidDataSelectDateInCalendarAndSity() { //выбор даты из календаря и города из списка
         $("[data-test-id='city'] input").setValue("Мо");
         $(".input__menu").find(byText("Москва")).should(visible, Duration.ofSeconds(15)).click();
-        String planningDate = generateDate(4, "dd.MM.yyyy");
-        $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);  //очищаем поле календаря
+        $("button.icon-button").click(); // Нажатие на иконку календаря
+        LocalDate planningDateLocal = LocalDate.now().plusDays(7); //добавляем к текущей дате неделю
+        String planningDate = planningDateLocal.format(DateTimeFormatter
+                .ofPattern("dd.MM.yyyy")); //кладем в переменную planningDate и приводим к нужному формату
+        String dayForClick = planningDateLocal.getDayOfMonth() + ""; //определяем только день месяца
+        $$(".calendar__day").filter(visible)
+                .find(text(dayForClick)).click(); //кликаем по найденному дню
         $("[data-test-id='name'] input").setValue("Иванов Иван Иванович");
         $("[data-test-id='phone'] input").setValue("+71234567890");
         $("[data-test-id='agreement']").click();
